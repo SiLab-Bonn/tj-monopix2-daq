c9f8fa9d47f1c28e61172e0f096fee7736980501
Christian Bespin <bespin@physik.uni-bonn.de>
2022-04-22 13:09:23 +0200

    ENH: Add analog scan

---
+++ tjmonopix2/scans/scan_analog.py
@@ -0,0 +1,58 @@
+#
+# ------------------------------------------------------------
+# Copyright (c) All rights reserved
+# SiLab, Institute of Physics, University of Bonn
+# ------------------------------------------------------------
+#
+
+from tqdm import tqdm
+ 
+from tjmonopix2.system.scan_base import ScanBase
+from tjmonopix2.scans.shift_and_inject import shift_and_inject, get_scan_loop_mask_steps
+from tjmonopix2.analysis import analysis, plotting
+
+scan_configuration = {
+    'start_column': 0,
+    'stop_column': 224,
+    'start_row': 0,
+    'stop_row': 512,
+}
+
+
+class AnalogScan(ScanBase):
+    scan_id = 'analog_scan'
+
+    def _configure(self, start_column=0, stop_column=512, start_row=0, stop_row=512, **_):
+        self.chip.masks['enable'][start_column:stop_column, start_row:stop_row] = True
+        self.chip.masks['injection'][start_column:stop_column, start_row:stop_row] = True
+        self.chip.masks['tdac'][start_column:stop_column, start_row:stop_row] = 0b100
+        self.chip.masks['hitor'][0, 0] = True
+
+        self.chip.masks.apply_disable_mask()
+        self.chip.masks.update(force=True)
+
+        self.chip.registers["ITHR"].write(50)
+        self.chip.registers["IDB"].write(100)
+
+        self.chip.registers["VL"].write(30)
+        self.chip.registers["VH"].write(150)
+        self.chip.registers["SEL_PULSE_EXT_CONF"].write(0)
+
+        self.daq.rx_channels['rx0']['DATA_DELAY'] = 14
+
+    def _scan(self, n_injections=100, **_):
+        pbar = tqdm(total=get_scan_loop_mask_steps(self), unit='Mask steps')
+        with self.readout(scan_param_id=0):
+            shift_and_inject(scan=self, n_injections=n_injections, pbar=pbar, scan_param_id=0)
+        pbar.close()
+
+        self.log.success('Scan finished')
+
+    def _analyze(self):
+        with analysis.Analysis(raw_data_file=self.output_filename + '.h5', **self.configuration['bench']['analysis']) as a:
+            a.analyze_data()
+
+
+if __name__ == "__main__":
+    with AnalogScan(scan_config=scan_configuration) as scan:
+        scan.start()
---
+++ tjmonopix2/scans/shift_and_inject.py
@@ -0,0 +1,54 @@
+#
+# ------------------------------------------------------------
+# Copyright (c) All rights reserved
+# SiLab, Institute of Physics, University of Bonn
+# ------------------------------------------------------------
+#
+
+'''
+    This module takes care of the mask shifting and injection of the supported chips
+    in order to keep actual scans cleaner.
+'''
+
+
+def shift_and_inject(scan, n_injections, pbar=None, scan_param_id=0, masks=['injection', 'enable'], pattern='default', cache=False, skip_empty=True):
+    ''' Regular mask shift and analog injection function.
+
+    Parameters:
+    ----------
+        scan : scan object
+            Scan object
+        n_injections : int
+            Number of injections per loop.
+        pbar : tqdm progressbar
+            Tqdm progressbar
+        scan_param_id : int
+            Scan parameter id of actual scan loop
+        masks : list
+            List of masks ('injection', 'enable', 'hitbus') which should be shifted during scan loop.
+        pattern : string
+            Injection pattern ('default', 'hitbus', ...)
+        cache : boolean
+            If True use mask caching for speedup. Default is False.
+        skip_empty : boolean
+            If True skip empty mask steps for speedup. Default is True.
+    '''
+    for fe, active_pixels in scan.chip.masks.shift(masks=masks, pattern=pattern, cache=cache, skip_empty=skip_empty):
+        if not fe == 'skipped':
+            scan.chip.inject(PulseStartCnfg=1, PulseStopCnfg=65, repetitions=n_injections, latency=1400)
+        if pbar is not None:
+            pbar.update(1)
+
+
+def get_scan_loop_mask_steps(scan, pattern='default'):
+    ''' Returns total number of mask steps for specific pattern
+
+    Parameters:
+    ----------
+        scan : scan object
+            Scan object
+        pattern : string
+            Injection pattern ('default', 'hitbus', ...)
+    '''
+
+    return scan.chip.masks.get_mask_steps(pattern=pattern)
