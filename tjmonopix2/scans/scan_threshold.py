e6c2a0b3b03040cba775577717d67a3f8fc5b60b
Maximilian Babeluk <maximilian@babeluk.at>
2022-06-09 16:50:29 +0200

    ENH: Imported scan_threshold from the development branch and fixed it.

---
+++ tjmonopix2/scans/scan_threshold.py
@@ -0,0 +1,75 @@
+#
+# ------------------------------------------------------------
+# Copyright (c) All rights reserved
+# SiLab, Institute of Physics, University of Bonn
+# ------------------------------------------------------------
+#
+
+from tjmonopix2.analysis import analysis
+from tjmonopix2.scans.shift_and_inject import (get_scan_loop_mask_steps,
+                                               shift_and_inject)
+from tjmonopix2.system.scan_base import ScanBase
+from tqdm import tqdm
+
+scan_configuration = {
+    'start_column': 480,
+    'stop_column': 512,
+    'start_row': 0,
+    'stop_row': 512,
+
+    'n_injections': 100,
+    'VCAL_HIGH': 150,
+    'VCAL_LOW_start': 140,
+    'VCAL_LOW_stop': 20,
+    'VCAL_LOW_step': -10
+}
+
+
+class ThresholdScan(ScanBase):
+    scan_id = 'threshold_scan'
+
+    def _configure(self, start_column=0, stop_column=512, start_row=0, stop_row=512, **_):
+        self.chip.masks['enable'][start_column:stop_column, start_row:stop_row] = True
+        self.chip.masks['injection'][start_column:stop_column, start_row:stop_row] = True
+        self.chip.masks['tdac'][start_column:stop_column, start_row:stop_row] = 0b100
+
+        self.chip.masks.apply_disable_mask()
+        self.chip.masks.update(force=True)
+
+        self.chip.registers["ITHR"].write(30)
+        self.chip.registers["IBIAS"].write(60)
+        self.chip.registers["ICASN"].write(8)
+        self.chip.registers["VCASP"].write(40)
+        self.chip.registers["VRESET"].write(100)
+        self.chip.registers["VCASC"].write(150)
+
+        self.daq.rx_channels['rx0']['DATA_DELAY'] = 14
+
+        self.chip.registers["SEL_PULSE_EXT_CONF"].write(0)
+
+    def _scan(self, n_injections=100, VCAL_HIGH=80, VCAL_LOW_start=80, VCAL_LOW_stop=40, VCAL_LOW_step=-1, **_):
+        """
+        Injects charges from VCAL_LOW_START to VCAL_LOW_STOP in steps of VCAL_LOW_STEP while keeping VCAL_HIGH constant.
+        """
+
+        self.chip.registers["VH"].write(VCAL_HIGH)
+        vcal_low_range = range(VCAL_LOW_start, VCAL_LOW_stop, VCAL_LOW_step)
+
+        pbar = tqdm(total=get_scan_loop_mask_steps(self) * len(vcal_low_range), unit='Mask steps')
+        for scan_param_id, vcal_low in enumerate(vcal_low_range):
+            self.chip.registers["VL"].write(vcal_low)
+
+            self.store_scan_par_values(scan_param_id=scan_param_id, vcal_high=VCAL_HIGH, vcal_low=vcal_low)
+            with self.readout(scan_param_id=scan_param_id):
+                shift_and_inject(scan=self, n_injections=n_injections, pbar=pbar, scan_param_id=scan_param_id)
+        pbar.close()
+        self.log.success('Scan finished')
+
+    def _analyze(self):
+        with analysis.Analysis(raw_data_file=self.output_filename + '.h5', **self.configuration['bench']['analysis']) as a:
+            a.analyze_data()
+
+
+if __name__ == "__main__":
+    with ThresholdScan(scan_config=scan_configuration) as scan:
+        scan.start()
