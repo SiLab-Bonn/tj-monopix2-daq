87d78dbc2c0fe2dd807c27904df756460d7dd0cd
Maximilian Babeluk <maximilian@babeluk.at>
2022-06-23 14:48:17 +0200

    merged offline_analysis and offline_plot to single file with various options

--- tjmonopix2/scans/offline_plot.py
+++ tjmonopix2/scans/offline_plot.py
@@ -8,6 +8,7 @@ import tables as tb
 import argparse
 
 from tables import NoSuchNodeError
+from tjmonopix2.analysis import analysis
 
 
 def calculate_mean_tot_map(hist_tot):
@@ -65,15 +66,20 @@ def plot_from_file(path_h5, output_dir):
     if output_dir is None:
         return
     basename = path.basename(output_dir)
-    h5file = tb.open_file(path_h5, mode="r", title='configuration_in')
-
     try:
+        h5file = tb.open_file(path_h5, mode="r", title='configuration_in')
+
         hist_occ = np.asarray(h5file.root.HistOcc)[:, :, 0]
         hist_tot = np.asarray(h5file.root.HistTot)
         avg_tot = calculate_mean_tot_map(hist_tot)
+
+        enable_mask = h5file.root.configuration_in.chip.masks.enable
+        registers = h5file.root.configuration_in.chip.registers
     except NoSuchNodeError:
         print("error in reading h5 file: probably not complete")
         return
+    finally:
+        h5file.close()
 
     prop_occ = {
         'colorbar_label': 'Occupancy',
@@ -91,41 +97,36 @@ def plot_from_file(path_h5, output_dir):
 
 
 parser = argparse.ArgumentParser()
-parser.add_argument('-f', action='store_true', default=None, help='replot even when plots already exist')
 parser.add_argument('-d', default='./output_data/module_0/chip_0', help='directory to find h5 files')
+parser.add_argument('-i', action='store_true', default=None, help='interpret h5 files that are not interpreted')
+parser.add_argument('-I', action='store_true', default=None, help='always re-interpret h5 files')
+parser.add_argument('-p', action='store_true', default=None, help='plot data from interpreted h5 files')
+parser.add_argument('-P', action='store_true', default=None, help='force replot of interpreted h5 files')
 args = parser.parse_args()
 
+# looks for uninterpreted files or reinterprates everything with -I
+if args.i or args.I:
+    for file in glob.glob(os.path.join(args.d, "*.h5")):
+        if file.endswith('_interpreted.h5'):
+            continue  # this is an interpreted file
+        file_interpreted = file.rsplit(".h5")[0]+"_interpreted.h5"
+        if path.isfile(file_interpreted):
+            if args.I:
+                os.remove(file_interpreted)
+            else:
+                continue
+        print('Analyzing file: '+path.basename(file))
+        with analysis.Analysis(raw_data_file=file) as a:
+            a.analyze_data()
 
-file = "output_data/module_0/chip_0/20220622_121944_analog_scan_interpreted.h5"
-for file in glob.glob(os.path.join(args.d, "*_interpreted.h5")):
-    output_dir = prepare_output_directory(file, force=args.f)
-    print(output_dir)
-
-    plot_from_file(file, output_dir)
+if args.p or args.P:
+    for file in glob.glob(os.path.join(args.d, "*_interpreted.h5")):
+        output_dir = prepare_output_directory(file, force=args.P)
+        if file:
+            print("Plotting: " + path.basename(file))
+            plot_from_file(file, output_dir)
 
 
 exit()
 
-h5file = tb.open_file("", mode="r", title='configuration_in')
 
-registers = h5file.root.configuration_in.chip.registers
-print(registers)
-print("---------------")
-
-register_readable = {}
-for x in registers.iterrows():
-    register_readable[x['register']] = x['value']
-print(register_readable)
-print("---------------")
-
-
-enable_mask = h5file.root.configuration_in.chip.masks.enable
-print(enable_mask)
-print("---------------")
-
-
-arr = np.asarray(enable_mask)
-
-print(type(arr))
-print(arr.shape)
-print("---------------")
