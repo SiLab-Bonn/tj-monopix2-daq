e0cb4945531e03bff2d37b836fd4ffdf1474f6c0
Lars Schall <lars.schall@uni-bonn.de>
2024-02-23 13:58:49 +0100

    REV: Keep scan_timeout feature for external trigger scan

--- tjmonopix2/scans/scan_ext_trigger.py
+++ tjmonopix2/scans/scan_ext_trigger.py
@@ -18,6 +18,7 @@ scan_configuration = {
     'start_row': 0,
     'stop_row': 512,
 
+    'scan_timeout': False,    # Timeout for scan after which the scan will be stopped, in seconds; if False no limit on scan time
     'max_triggers': 1000000,  # Number of maximum received triggers after stopping readout, if False no limit on received trigger
 
     'tot_calib_file': None    # path to ToT calibration file for charge to e⁻ conversion, if None no conversion will be done
@@ -29,31 +30,51 @@ class ExtTriggerScan(ScanBase):
 
     stop_scan = threading.Event()
 
-    def _configure(self, max_triggers=1000, start_column=0, stop_column=512, start_row=0, stop_row=512, **_):
+    def _configure(self, scan_timeout=False, max_triggers=1000, start_column=0, stop_column=512, start_row=0, stop_row=512, **_):
         self.log.info('External trigger scan needs TLU running!')
+
+        if scan_timeout and max_triggers:
+            self.log.warning('You should only use one of the stop conditions at a time.')
 
         self.chip.masks['enable'][start_column:stop_column, start_row:stop_row] = True
         self.chip.masks.apply_disable_mask()
         self.chip.masks.update()
 
         self.daq.configure_tlu_veto_pulse(veto_length=500)
-        self.daq.configure_tlu_module(max_triggers=max_triggers)
+        if max_triggers:
+            self.daq.configure_tlu_module(max_triggers=max_triggers)
 
-    def _scan(self, max_triggers=1000, **_):
-        self.pbar = tqdm(total=max_triggers, unit=' Triggers')
+    def _scan(self, scan_timeout=False, max_triggers=1000, **_):
+        def timed_out():
+            if scan_timeout:
+                current_time = time.time()
+                if current_time - start_time > scan_timeout:
+                    self.log.info('Scan timeout was reached')
+                    return True
+            return False
+
+        if scan_timeout:
+            self.pbar = tqdm(total=scan_timeout, unit='')  # [s]
+        elif max_triggers:
+            self.pbar = tqdm(total=max_triggers, unit=' Triggers')
+        start_time = time.time()
 
         with self.readout():
             self.stop_scan.clear()
             self.daq.enable_tlu_module()
 
-            while not self.stop_scan.is_set():
+            while not (self.stop_scan.is_set() or timed_out()):
                 try:
-                    triggers = self.daq.get_trigger_counter()
+                    if max_triggers:
+                        triggers = self.daq.get_trigger_counter()
                     time.sleep(1)
 
                     # Update progress bar
                     try:
-                        self.pbar.update(self.daq.get_trigger_counter() - triggers)
+                        if scan_timeout:
+                            self.pbar.update(1)
+                        elif max_triggers:
+                            self.pbar.update(self.daq.get_trigger_counter() - triggers)
                     except ValueError:
                         pass
 
