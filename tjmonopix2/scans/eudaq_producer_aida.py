6c77177df1608af17edb6b47a949db1dba6f0559
Christian Bespin <cbespin@uni-bonn.de>
2025-01-27 15:07:42 +0100

    Codestyle and more pythonic

--- tjmonopix2/scans/eudaq_producer_aida.py
+++ tjmonopix2/scans/eudaq_producer_aida.py
@@ -1,49 +1,46 @@
-import time
 import argparse
-import pyeudaq
 import socket
 import threading
+import time
+
+import pyeudaq
 from tjmonopix2.scans.scan_ext_trigger import ExtTriggerScan
 from tjmonopix2.system import logger
 
 
-def IsHostReachable(host="192.168.10.23",port=24,timeout=20) -> bool: #:TODO komentar
-        try:
-            socket.setdefaulttimeout(timeout)
-            with socket.create_connection((host, port)):
-                return True
-        except (socket.timeout, socket.error):
-            return False
-        
+def host_reachable(host="192.168.10.23", port=24, timeout=20):
+    try:
+        socket.setdefaulttimeout(timeout)
+        with socket.create_connection((host, port)):
+            return True
+    except (socket.timeout, socket.error):
+        return False
 
 
 class EudaqProducerAida(pyeudaq.Producer):
-    scan_id = 'ext_trigger_scan_producer' 
+    scan_id = 'eudaq_scan' 
 
     def __init__(self, name, runctrl):
         pyeudaq.Producer.__init__(self, name, runctrl)
         self.log = logger.setup_derived_logger(self.__class__.__name__)
         self.is_running = 0
-        self.ini = None
         self.scan = None
         self.conf = {}
         self.thread_scan = None
         self.reg_config = {}
         self.init_register_vals = {}
-        self.BDAQBoardTimeout=10
-
+        self.BDAQBoardTimeout = 10
 
     def __del__(self):
          if self.is_running:
              self.scan.close()
 
     def DoInitialise(self):
-        #self.ini = self.GetInitConfiguration()
         self.log.info("Initialization completed")
 
     def DoConfigure(self):
         self.log.info("Probing if power is up")
-        if IsHostReachable("192.168.10.23",24,self.BDAQBoardTimeout):
+        if host_reachable("192.168.10.23", 24, self.BDAQBoardTimeout):
             try:
                 self.log.info("Power is up")
                 self.scan = ExtTriggerScan() 
@@ -55,49 +52,42 @@ class EudaqProducerAida(pyeudaq.Producer):
         else:
             self.log.error("Initialization failed")
             raise RuntimeError("BDAQ board unreachable")
-        
+
         eudaqConfig = self.GetConfiguration() 
 
-        self.conf["start_column"]=int(eudaqConfig.Get("start_column","0"))
-
-        self.conf["stop_column"]=int(eudaqConfig.Get("stop_column","512"))
-
-        self.conf["start_row"]=int(eudaqConfig.Get("start_row","0")) 
-
-        self.conf["stop_row"]=int(eudaqConfig.Get("stop_row","512"))
-
-        self.conf["max_triggers"]=int(eudaqConfig.Get("max_triggers","1000")) 
+        self.conf["start_column"] = int(eudaqConfig.Get("start_column", "0"))
+        self.conf["stop_column"] = int(eudaqConfig.Get("stop_column", "512"))
+        self.conf["start_row"] = int(eudaqConfig.Get("start_row", "0")) 
+        self.conf["stop_row"] = int(eudaqConfig.Get("stop_row", "512"))
+        self.conf["max_triggers"] = int(eudaqConfig.Get("max_triggers", "1000")) 
 
         if eudaqConfig.Get("scan_timeout").lower() == "false":
-            self.conf["scan_timeout"]=False
+            self.conf["scan_timeout"] = False
         elif eudaqConfig.Get("scan_timeout").lower() == "true":
-            self.conf["scan_timeout"]=True
-        
-        if eudaqConfig.Get("chip_config_file").lower()=="none":
-            self.conf["chip_config_file"]=None
-        else:
-            pass
-        
+            self.conf["scan_timeout"] = True
+
+        if eudaqConfig.Get("chip_config_file").lower() == "none":
+            self.conf["chip_config_file"] = None
+
         configurable_regs = ['VL', 'VH', 'ITHR', 'IBIAS', 'VCASP', 'ICASN', 'VRESET', 'VCLIP', 'IDB', 'IDEL', 'VCASC']
         for reg in configurable_regs:
             self.reg_config[reg] = self.GetConfigItem(reg)
-        
 
-        self.scan.scan_config=self.conf
+        self.scan.scan_config = self.conf
         print(self.reg_config)
         try:
             self.scan.configure()
             self.log.info("Configuration completed")
         except Exception as e:
             raise e
-        
+
         for reg in self.reg_config.keys():            
             reg_val = self.reg_config[reg]
             reg_val = reg_val.replace(',', '.')
             if reg_val:
                 reg_val_int = int(float(reg_val))
                 reg_val_float = float(reg_val)
-                
+
                 if (reg_val_float - reg_val_int) > 0.001:
                     print('Contains, ', reg_val, ' in ', reg)
                     self.current_scan_register = reg
@@ -109,7 +99,6 @@ class EudaqProducerAida(pyeudaq.Producer):
                     self.scan.chip.registers[reg].write(int(float(reg_val)))
 
         self.scan.chip.registers['SEL_PULSE_EXT_CONF'].write(0)
-        
 
     def DoStatus(self):
         if self.last_exception is not None:
@@ -128,14 +117,12 @@ class EudaqProducerAida(pyeudaq.Producer):
             self.DoConfigure()
         if not self.scan.scan_config["max_triggers"]:
             self.scan.daq.configure_tlu_module(max_triggers=False, aidamode=True)
-            
+
         self.is_running = 1
         self.thread_scan = threading.Thread(target=self.scan.scan)
         self.thread_trigger = threading.Thread(target=self.SendTriggerNumber)
         self.thread_scan.start()
         self.thread_trigger.start()
-        
-       
 
     def DoStopRun(self):
         if self.is_running: 
@@ -148,7 +135,6 @@ class EudaqProducerAida(pyeudaq.Producer):
             self.SetStatusTag("TriggerN", "0")
             self.log.info("Scan was stopped")
 
-
     def DoReset(self):
         if self.is_running:
             self.is_running=0
@@ -158,27 +144,24 @@ class EudaqProducerAida(pyeudaq.Producer):
             self.SetStatusTag("TriggerN", "0")
         if self.scan is not None:
             self.scan.close()
-        self.scan=None
+        self.scan = None
         self.log.info("Reset completed")
-        
-
 
     def DoTerminate(self):
         if self.is_running:
-            self.is_running=0
+            self.is_running = 0
             self.scan.stop_scan.set()
             self.thread_scan.join()
             self.thread_trigger.join()
             self.scan.close()
-        self.scan=None
+        self.scan = None
         self.log.info("Terminated")
-        
+
     def SendTriggerNumber(self):
         while self.is_running:
             self.SetStatusTag("TriggerN", str(self.scan.daq.get_trigger_counter()))
             time.sleep(1)
 
-    
 
 if __name__ == '__main__':
     # Parse program arguments
@@ -200,5 +183,3 @@ if __name__ == '__main__':
     time.sleep(2)
     while producer.IsConnected():
         time.sleep(1)
-        
-        
