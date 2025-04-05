25576ea44a72de4bc68d2cd0c2e04f12e81d29d3
karamel <bernhard.pilsl@oeaw.ac.at>
2025-04-05 11:17:32 +0200

    find masked pixels for rows and cols

--- tjmonopix2/scans/find_masked_pixel_analysis.py
+++ tjmonopix2/scans/find_masked_pixel_analysis.py
@@ -3,6 +3,7 @@ import tables as tb
 from pathlib import Path
 import yaml
 import glob
+import argparse
 
 #
 # Finds the lates noise occupancy scan in a given folder and prints the disabled pixels to terminal
@@ -25,16 +26,19 @@ def find_latest_file(path: str, index: str):
         key=lambda item: item.stat().st_ctime,
     )
 
+parser = argparse.ArgumentParser(description="Convert hit table to be compatible with corryvreckan EventLoaderHDF5.")
+parser.add_argument("run", type=int, help="Number of run for which the mask file is created.")
+args = parser.parse_args()
 
-run_no=1634
-
+if args.run:
+    run_no = int(args.run)
 # # Standard usage
-folder_path = '/home/bellevtx01/tb2025d/desy-tb-2025/data/dut/module_0/chip_0'
+folder_path = '/media/testbeam1/tb2025c/tb2025d/desy-tb-2025/data/dut/module_0/chip_0/'
 # filepath_in = find_latest_file(folder_path, 'noise_occupancy_scan_interpreted.h5')
 
 # # Select the wanted file -- COMMENT FOR STANDARD USAGE
 # filepath_in = f"/home/bellevtx01/tb2025d/desy-tb-2025/data/dut/module_0/chip_0/run*{run_no}_*_ext_trigger_scan.h5"
-filepath_in = f"/media/bellevtx01/tb2025d/desy-tb-2025/data/dut/module_0/chip_0/run00{run_no}_*_ext_trigger_scan.h5"
+filepath_in = f"/media/testbeam1/tb2025c/tb2025d/desy-tb-2025/data/dut/module_0/chip_0/run00{run_no}_*_ext_trigger_scan.h5"
 
 # Expand the wildcard using glob
 files = glob.glob(filepath_in)
@@ -48,9 +52,15 @@ print(f'Using File: {files[0]}')
 
 
 print('Using File: %s' %filepath_in)
-
 with tb.open_file(files[0], "r") as in_file:
     pixel_mask = in_file.root.configuration_out.chip.use_pixel[:]
+
+    start_col = int(in_file.root.configuration_out.scan.scan_config[2][1])
+    stop_col = int(in_file.root.configuration_out.scan.scan_config[3][1])
+    start_row = int(in_file.root.configuration_out.scan.scan_config[4][1])
+    stop_row = int(in_file.root.configuration_out.scan.scan_config[5][1])
+
+
     # pixel_mask = in_file.root.configuration_out.chip.masks.enable[:]
 
 print('--- Disabled Pixels ---')
@@ -70,7 +80,13 @@ with open(folder_path + f"/run00{run_no}_masked_pixels.txt", 'w') as file:
         col = disabled_pixels[0,i]
         
         file.write(f'p {col} {row}\n')
-
+    
+    disabled_cols = [i for i in range(512) if not (start_col <= i <= stop_col)]
+    for i in disabled_cols:
+        file.write(f'c {i}\n')
+    disabled_rows = [i for i in range(512) if not (start_row <= i <= stop_row)]
+    for i in disabled_rows:
+        file.write(f'r {i}\n')
 
 # # Enumerating only usage -- COMMENT FOR STANDARD USAGE
 # for i in range(np.shape(disabled_pixels)[1]):
