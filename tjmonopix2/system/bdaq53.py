233f1490b713f5419f74fac2a89cbff3e1437c76
Christian Bespin <cbespin@uni-bonn.de>
2025-05-19 13:07:22 +0200

    STY: More codestyle

--- tjmonopix2/system/bdaq53.py
+++ tjmonopix2/system/bdaq53.py
@@ -285,12 +285,12 @@ class BDAQ53(Dut):
         if aidamode:
             self['tlu']["TRIGGER_MODE"] = 2
             self['tlu']["TRIGGER_LOW_TIMEOUT"] = 4
-            self['tlu']["TRIGGER_HANDSHAKE_ACCEPT_WAIT_CYCLES"] = 1 
+            self['tlu']["TRIGGER_HANDSHAKE_ACCEPT_WAIT_CYCLES"] = 1
             self['tlu']['EN_TLU_RESET_TIMESTAMP'] = 1
         else:
             self['tlu']["TRIGGER_MODE"] = 3
             self['tlu']["TRIGGER_LOW_TIMEOUT"] = 0
-            self['tlu']["TRIGGER_HANDSHAKE_ACCEPT_WAIT_CYCLES"] = 5 
+            self['tlu']["TRIGGER_HANDSHAKE_ACCEPT_WAIT_CYCLES"] = 5
 
     def get_tlu_erros(self):
         return (self['tlu']['TRIGGER_LOW_TIMEOUT_ERROR_COUNTER'], self['tlu']['TLU_TRIGGER_ACCEPT_ERROR_COUNTER'])
