db38a5065d73ab1ea9d7f4393ceb8d1b35a55217
rpartzsch <rasmus.partzsch@hotmail.com>
2025-09-04 13:45:58 +0200

    enh: testbench yaml hack for constellation

--- tjmonopix2/constellation_tj/tj_satellite.py
+++ tjmonopix2/constellation_tj/tj_satellite.py
@@ -3,6 +3,11 @@ from constellation.core.satellite import Satellite
 import time
 from tjmonopix2.scans.scan_ext_trigger import ExtTriggerScan
 import threading
+import yaml
+import os
+
+PROJECT_FOLDER = os.path.join(os.path.dirname(__file__), '..')
+TESTBENCH_DEFAULT_FILE = os.path.join(PROJECT_FOLDER, 'testbench.yaml')
 
 class TJ(Satellite):
     def __init__(self, *args, **kwargs):
@@ -12,7 +17,17 @@ class TJ(Satellite):
         configuration = config.get_dict()
         if configuration["tot_calib_file"] == "None":
             configuration["tot_calib_file"] = None
+        if configuration["chip_config_file"] == "None":
+            configuration["chip_config_file"] = None
         self.config_file = configuration
+
+        with open(TESTBENCH_DEFAULT_FILE, 'r') as f:
+            conf = yaml.full_load(f)
+            conf['general']['output_directory'] = configuration['output_folder']
+            conf['modules']['module_0']['chip_0']['chip_config_file'] = configuration['chip_config_file']
+
+        with open(TESTBENCH_DEFAULT_FILE, 'w') as f:
+            yaml.dump(conf, f)
 
         self.ext_trg_scan = ExtTriggerScan(scan_config=self.config_file)
         self.ext_trg_scan.init()
